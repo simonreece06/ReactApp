40160819c8c830e78c7828e893bfe3d3202dbbeb
Simon Reece <simonreece06@hotmail.com>
2026-08-17 00:29:14 +0100

    BASIC Navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,27 @@
+import "./NavBar.css";
+
+
+
 const NavBar = () => {
-    return <nav>Nav</nav>;
-};
+    return (
+        <nav className="navbar">
+            <div>
+                <h1>RedditClient</h1>
+            </div>
+            <div className="search-bar">
+                <input
+                  type="search"
+                  placeholder="search me"
+                />
+                <button>Search</button>
+            </div>
+            <div className="nav-buttons">
+                <button>Log in</button>
+                <button>Sign up</button>
+            </div>
+        </nav>        
+    );
 
-
+}
 
 export default NavBar
