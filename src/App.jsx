5cc4340973c2316917deac675467cb1a56a8442c
Simon Reece <simonreece06@hotmail.com>
2026-08-22 01:17:55 +0100

    mapping posts

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,20 @@ import './App.css'
 import NavBar from './components/NavBar/NavBar.jsx';
 import Post from './components/Post/Post.jsx';
 
-import dummyPostData from './components/Post/dummyPostData.jsx';
+import { dummyFeed } from './components/Post/dummyPostData.jsx';
 
 function App() {
 
-  const post = {
-    data: dummyPostData
-  };
+
 
 
 
   return (
     <div>
       <NavBar />
-      <Post post={post.data} />
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      {dummyFeed.map((post) => (
+        <Post key={post.title} post={post} />
+      ))}
 
 
     </div>
--- src/components/Post/dummyPostData.jsx
+++ src/components/Post/dummyPostData.jsx
@@ -8,4 +8,31 @@ const oneDummyPost = {
     created: 1755700000
 }
 
-export default oneDummyPost;
+//now we attempt dummy data of 3 posts, for prep for running a feed
+
+const dummyFeed = [
+    oneDummyPost,
+    {
+        title: 'my second post',
+        selftext: 'What did you say about me? I have you know I ...',
+        author: 'Seal team 6 operative',
+        score: 100,
+        subreddit: 'casualuk',
+        num_comments: 100,
+        created: 1755700000
+    },
+    {
+        title: 'my third post',
+        selftext: 'They say I\'m dead.....',
+        author: 'Johnny',
+        score: 5,
+        subreddit: 'videos',
+        num_comments: 5,
+        created: 1755700000
+    }
+
+]
+
+
+export { dummyFeed };
+export { oneDummyPost };
